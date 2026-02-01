f487a1289a7b70a966c59599de4147cf2053373e
SanjayS66 <sanjay702594@gmail.com>
2026-02-01 14:55:05 +0530

    to pull

--- RPLIDAR_RCUP/launch/view_sllidar_a2m8_launch.py
+++ RPLIDAR_RCUP/launch/view_sllidar_a2m8_launch.py
@@ -14,12 +14,7 @@ def generate_launch_description():
     frame_id = LaunchConfiguration('frame_id', default='laser')
     inverted = LaunchConfiguration('inverted', default='true')
     angle_compensate = LaunchConfiguration('angle_compensate', default='true')
-<<<<<<< HEAD
-    # Available scan modes: 'Standard', 'Express', 'Boost', 'Sensitivity'
-    scan_mode = LaunchConfiguration('scan_mode', default='Standard')
-=======
     scan_mode = LaunchConfiguration('scan_mode', default='Boost')
->>>>>>> c08b57bedbf8fe37f296aafaad7a51a286fda3e0
 
     rviz_config_dir = os.path.join(
         get_package_share_directory('sllidar_ros2'),
